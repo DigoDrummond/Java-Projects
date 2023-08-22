eaeaa9964a67b468df67d7d701173d774cb10a6f
DigoDrummond <rodrigodm2810@gmail.com>
2023-08-22 15:13:23 -0300

    Testing 2

--- Eclipse_GitHub/src/Main.java
+++ Eclipse_GitHub/src/Main.java
@@ -1,17 +1,17 @@
 
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
 
+	public static Scanner sc = new Scanner(System.in);
+
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Scanner sc = new Scanner(System.in);
-		
+
 		int a = sc.nextInt();
 		int b = sc.nextInt();
-		
-		System.out.println(a+b);
-		
+
+		System.out.println(a + b);
+
 		sc.close();
 	}
 
