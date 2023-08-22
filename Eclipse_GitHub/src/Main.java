904b38cd6fa7bc12a2cdd351bccedf69c3e4b915
DigoDrummond <rodrigodm2810@gmail.com>
2023-08-22 14:51:08 -0300

    Testing commit

--- Eclipse_GitHub/src/Main.java
+++ Eclipse_GitHub/src/Main.java
@@ -1,9 +1,18 @@
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("teste");
+		Scanner sc = new Scanner(System.in);
+		
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		
+		System.out.println(a+b);
+		
+		sc.close();
 	}
 
 }
